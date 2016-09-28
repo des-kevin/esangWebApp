154063171ca961822e9af74212488c0a969c198d
des-kevin <des-kevin@122-117-101-223.hinet-ip.hinet.net>
2016-09-29 00:27:51 +0800

    save all.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("test app");
+/*
+ * Name : Esangtek manager System
+ * Date  : 2016-09-28
+ * Author: kevin lin
+ * 
+ */
+
+var requirejs=require("requirejs");
+
+
+console.log(" Starting Esangtek Manager System! ");
+
+require([
+    "systems/esObject.js",
+    "systems/esController.js",
+],function(){
+    
+});
 
--- systems/esObject.js
+++ systems/esObject.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Name : Esangtek manager System
+ * Date  : 2016-09-28
+ * Author: kevin lin
+ * 
  */
 
+
+var esObject=(function(obj){
+    
+    return obj;
+})(esObject || {});
 
